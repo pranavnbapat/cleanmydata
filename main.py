877c2af4a890f254df786ea25aa10f8990068c8e
pranavnbapat <pranav.g33k@gmail.com>
2022-10-29 13:29:16 +0200

    dropna enhancements added

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+import pandas as pd
+
 from cleanmydata.functions import *
 
-df = pd.read_csv('data/training.1600000.processed.noemoticon.csv', encoding='ISO-8859-1', dtype='unicode')
-print(df.head(10))
-df = clean_data(lst=[14, 15], data=df, column='comment', save=False, name='my custome name')
-print(df.head(10))
-print(df.columns)
-print(df.shape)
+# df = pd.read_csv('data/training.1600000.processed.noemoticon.csv', encoding='ISO-8859-1', dtype='unicode')
+# print(df.head(10))
+# print(df.columns)
+# print(df.shape)
+# df = clean_data(lst={2, 99}, data=df, column='comment', save=False, name='my custome name')
+# print(df.head(10))
+# print(df.columns)
+# print(df.shape)
 
-# my_data = "Pranav #Bapat pranav.g33k@gmail.com Hello"
-# my_data = clean_data(lst=[2, 4, 9], data=my_data)
-# print(my_data)
+
+my_data = "pranav.g33k@gmail.com Pranav #Hello Bapat"
+print(my_data)
+my_data = clean_data(lst={1, 2}, data=my_data, column='comment')
+print(my_data)
