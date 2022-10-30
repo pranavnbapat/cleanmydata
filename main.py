9a2bc660c8988892ca5b5bd8c2688c2c675bf27a
pranavnbapat <pranav.g33k@gmail.com>
2022-10-30 14:52:36 +0100

    main file removed

--- main.py
+++
@@ -1,8 +0,0 @@
-from cleanmydata.functions import *
-
-df = pd.read_csv('data/training.1600000.processed.noemoticon.csv', encoding='ISO-8859-1', dtype='unicode')
-df = clean_data(lst={2, 3, 4, 5, 6, 7, 16}, data=df, column='comment')
-print(df.head())
-print(df.columns)
-print(df.shape)
-
