d03737ee8224c0800f4f6506cf92acbbf68d0188
pranavnbapat <pranav.g33k@gmail.com>
2022-11-04 18:31:27 +0100

    contextual spell check, contractions added

--- cleanmydata/spell_check.py
+++ cleanmydata/spell_check.py
@@ -6,3 +6,8 @@ import contextualSpellCheck
 
 nlp = spacy.load('en_core_web_sm')
 contextualSpellCheck.add_to_pipe(nlp)
+doc = nlp('Mind must be the manster of the body')
+
+print(doc._.performed_spellCheck) #Should be True
+print(doc._.outcome_spellCheck)
+print(doc._.score_spellCheck)
